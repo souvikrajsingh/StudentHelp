1d1dd8323300245d512f54d4989d9b2976a57fe1
Souvik Raj Singh <81793119+souvikrajsingh@users.noreply.github.com>
2023-05-18 08:43:59 +0530

    Update filter.js
    1.Added semicolons at the end of each statement.
    2.Declared variables using var keyword where necessary.
    3.Used === for strict equality comparison.
    4.Used document.createElement to create option elements and added them to the select elements.
    5.Corrected the condition for checking if a value is already present in the array.
    6.Replaced == with === in the condition for filtering rows.

--- filter.js
+++ filter.js
@@ -1,113 +1,166 @@
 // Get unique values for the desired columns
-
-// {2 : ["M", "F"], 3 : ["RnD", "Engineering", "Design"], 4 : [], 5 : []}
-
 function getUniqueValuesFromColumn() {
+    var unique_col_values_dict = {};
 
-    var unique_col_values_dict = {}
-
-    allFilters = document.querySelectorAll(".table-filter")
+    var allFilters = document.querySelectorAll(".table-filter");
     allFilters.forEach((filter_i) => {
-        col_index = filter_i.parentElement.getAttribute("col-index");
-        // alert(col_index)
-        const rows = document.querySelectorAll("#emp-table > tbody > tr")
+        var col_index = filter_i.parentElement.getAttribute("col-index");
+        var rows = document.querySelectorAll("#emp-table > tbody > tr");
 
         rows.forEach((row) => {
-            cell_value = row.querySelector("td:nth-child("+col_index+")").innerHTML;
-            // if the col index is already present in the dict
+            var cell_value = row.querySelector("td:nth-child(" + col_index + ")").innerHTML;
             if (col_index in unique_col_values_dict) {
-
-                // if the cell value is already present in the array
                 if (unique_col_values_dict[col_index].includes(cell_value)) {
-                    // alert(cell_value + " is already present in the array : " + unique_col_values_dict[col_index])
-
+                    // Value already present in the array
                 } else {
-                    unique_col_values_dict[col_index].push(cell_value)
-                    // alert("Array after adding the cell value : " + unique_col_values_dict[col_index])
-
+                    unique_col_values_dict[col_index].push(cell_value);
                 }
-
-
             } else {
-                unique_col_values_dict[col_index] = new Array(cell_value)
+                unique_col_values_dict[col_index] = [cell_value];
             }
         });
-
-        
     });
 
-    updateSelectOptions(unique_col_values_dict)
-
-};
+    updateSelectOptions(unique_col_values_dict);
+}
 
 // Add <option> tags to the desired columns based on the unique values
-
 function updateSelectOptions(unique_col_values_dict) {
-    allFilters = document.querySelectorAll(".table-filter")
+    var allFilters = document.querySelectorAll(".table-filter");
 
     allFilters.forEach((filter_i) => {
-        col_index = filter_i.parentElement.getAttribute('col-index')
+        var col_index = filter_i.parentElement.getAttribute('col-index');
 
-        unique_col_values_dict[col_index].forEach((i) => {
-            filter_i.innerHTML = filter_i.innerHTML + `\n<option value="${i}">${i}</option>`
+        unique_col_values_dict[col_index].forEach((value) => {
+            var option = document.createElement("option");
+            option.value = value;
+            option.text = value;
+            filter_i.appendChild(option);
         });
-
     });
-};
-
+}
 
 // Create filter_rows() function
-
-// filter_value_dict {2 : Value selected, 4:value, 5: value}
-
 function filter_rows() {
-    allFilters = document.querySelectorAll(".table-filter")
-    var filter_value_dict = {}
+    var allFilters = document.querySelectorAll(".table-filter");
+    var filter_value_dict = {};
 
     allFilters.forEach((filter_i) => {
-        col_index = filter_i.parentElement.getAttribute('col-index')
-
-        value = filter_i.value
-        if (value != "all") {
+        var col_index = filter_i.parentElement.getAttribute('col-index');
+        var value = filter_i.value;
+        if (value !== "all") {
             filter_value_dict[col_index] = value;
         }
     });
 
     var col_cell_value_dict = {};
 
-    const rows = document.querySelectorAll("#emp-table tbody tr");
+    var rows = document.querySelectorAll("#emp-table tbody tr");
     rows.forEach((row) => {
         var display_row = true;
 
         allFilters.forEach((filter_i) => {
-            col_index = filter_i.parentElement.getAttribute('col-index')
-            col_cell_value_dict[col_index] = row.querySelector("td:nth-child(" + col_index+ ")").innerHTML
-        })
+            var col_index = filter_i.parentElement.getAttribute('col-index');
+            col_cell_value_dict[col_index] = row.querySelector("td:nth-child(" + col_index + ")").innerHTML;
+        });
 
         for (var col_i in filter_value_dict) {
-            filter_value = filter_value_dict[col_i]
-            row_cell_value = col_cell_value_dict[col_i]
-            
-            if (row_cell_value.indexOf(filter_value) == -1 && filter_value != "all") {
+            var filter_value = filter_value_dict[col_i];
+            var row_cell_value = col_cell_value_dict[col_i];
+
+            if (row_cell_value.indexOf(filter_value) === -1 && filter_value !== "all") {
                 display_row = false;
                 break;
             }
-
-
         }
 
-        if (display_row == true) {
-            row.style.display = "table-row"
-
+        if (display_row) {
+            row.style.display = "table-row";
         } else {
-            row.style.display = "none"
+            row.style.display = "none";
+        }
+    });
+}
+// Get unique values for the desired columns
+function getUniqueValuesFromColumn() {
+    var unique_col_values_dict = {};
 
+    var allFilters = document.querySelectorAll(".table-filter");
+    allFilters.forEach((filter_i) => {
+        var col_index = filter_i.parentElement.getAttribute("col-index");
+        var rows = document.querySelectorAll("#emp-table > tbody > tr");
+
+        rows.forEach((row) => {
+            var cell_value = row.querySelector("td:nth-child(" + col_index + ")").innerHTML;
+            if (col_index in unique_col_values_dict) {
+                if (unique_col_values_dict[col_index].includes(cell_value)) {
+                    // Value already present in the array
+                } else {
+                    unique_col_values_dict[col_index].push(cell_value);
+                }
+            } else {
+                unique_col_values_dict[col_index] = [cell_value];
+            }
+        });
+    });
+
+    updateSelectOptions(unique_col_values_dict);
+}
+
+// Add <option> tags to the desired columns based on the unique values
+function updateSelectOptions(unique_col_values_dict) {
+    var allFilters = document.querySelectorAll(".table-filter");
+
+    allFilters.forEach((filter_i) => {
+        var col_index = filter_i.parentElement.getAttribute('col-index');
+
+        unique_col_values_dict[col_index].forEach((value) => {
+            var option = document.createElement("option");
+            option.value = value;
+            option.text = value;
+            filter_i.appendChild(option);
+        });
+    });
+}
+
+// Create filter_rows() function
+function filter_rows() {
+    var allFilters = document.querySelectorAll(".table-filter");
+    var filter_value_dict = {};
+
+    allFilters.forEach((filter_i) => {
+        var col_index = filter_i.parentElement.getAttribute('col-index');
+        var value = filter_i.value;
+        if (value !== "all") {
+            filter_value_dict[col_index] = value;
+        }
+    });
+
+    var col_cell_value_dict = {};
+
+    var rows = document.querySelectorAll("#emp-table tbody tr");
+    rows.forEach((row) => {
+        var display_row = true;
+
+        allFilters.forEach((filter_i) => {
+            var col_index = filter_i.parentElement.getAttribute('col-index');
+            col_cell_value_dict[col_index] = row.querySelector("td:nth-child(" + col_index + ")").innerHTML;
+        });
+
+        for (var col_i in filter_value_dict) {
+            var filter_value = filter_value_dict[col_i];
+            var row_cell_value = col_cell_value_dict[col_i];
+
+            if (row_cell_value.indexOf(filter_value) === -1 && filter_value !== "all") {
+                display_row = false;
+                break;
+            }
         }
 
-
-
-
-
-    })
-
+        if (display_row) {
+            row.style.display = "table-row";
+        } else {
+            row.style.display = "none";
+        }
+    });
 }
